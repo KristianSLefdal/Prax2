6184bd29e3c89dd1b54325e6df1a7eb3dcdafd17
KristianSLefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-29 11:24:46 +0100

    Add files via upload

---
+++ dimentions.py
@@ -0,0 +1,8 @@
+length = int(input('input a number:' ))
+print (length)
+breadth = int(input('input a number:'))
+print (breadth)
+height = int(input('input aa number:'))
+print (height)
+volumofbox = length*breadth*height
+print (volumofbox)
