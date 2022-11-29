4bd4fb624e6732eeba4e840b3efb759603497893
KristianSLefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-29 11:27:34 +0100

    Delete dimentions.py

--- dimentions.py
+++
@@ -1,8 +0,0 @@
-length = int(input('input a number:' ))
-print (length)
-breadth = int(input('input a number:'))
-print (breadth)
-height = int(input('input aa number:'))
-print (height)
-volumofbox = length*breadth*height
-print (volumofbox)
