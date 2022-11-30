60e76185b489bdbed7b96f0fb4a7abb7c7b7e2eb
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-30 13:35:04 +0100

    Add files via upload

---
+++ splitandjoin.py
@@ -0,0 +1,4 @@
+#Given the below variable and string assignment, use it in your Python script and print the indexes in the following order 16,15,4,17,6,12,11,18. You must use the Python split() function to split the string.
+strmessage = "what,is,not,good,I,will,be,you,ok,Noroff,student,Python,a,awesome,pretty,knew,who,would,programmer"
+strmessage = strmessage.split(",")
+print(strmessage[16], strmessage[15], strmessage[4], strmessage[17], strmessage[6], strmessage[12], strmessage[11], strmessage[18])
