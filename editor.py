6f938b6d955070c2c68548190e3cc349af88b774
Kristian Skjevik lefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-30 15:30:12 +0100

    Add files via upload

---
+++ editor.py
@@ -0,0 +1,62 @@
+#2.	Create a script named editor.py. In this script, prompt the user to enter a string of text. The following needs to happen until the user decides to exit the program, by pressing "e":
+
+sentence = input('string:')
+Userchoice = input('1. Uppercase\n2. Lowercase\n3. Titlecase\n4. Remove front and back whitespaces\n5. Exit\n')
+if Userchoice == '1':
+    print(sentence.upper())
+elif Userchoice == '2':
+    print(sentence.lower())
+elif Userchoice == '3':
+    print(sentence.title())
+elif Userchoice == '4':
+    print(sentence.strip())
+elif Userchoice == '5':
+    exit()
+sentence = input('string:')
+Userchoice = input('1. Uppercase\n2. Lowercase\n3. Titlecase\n4. Remove front and back whitespaces\n5. Exit\n')
+if Userchoice == '1':
+    print(sentence.upper())
+elif Userchoice == '2':
+    print(sentence.lower())
+elif Userchoice == '3':
+    print(sentence.title())
+elif Userchoice == '4':
+    print(sentence.strip())
+elif Userchoice == '5':
+    exit()
+sentence = input('string:')
+Userchoice = input('1. Uppercase\n2. Lowercase\n3. Titlecase\n4. Remove front and back whitespaces\n5. Exit\n')
+if Userchoice == '1':
+    print(sentence.upper())
+elif Userchoice == '2':
+    print(sentence.lower())
+elif Userchoice == '3':
+    print(sentence.title())
+elif Userchoice == '4':
+    print(sentence.strip())
+elif Userchoice == '5':
+    exit()
+sentence = input('string:')
+Userchoice = input('1. Uppercase\n2. Lowercase\n3. Titlecase\n4. Remove front and back whitespaces\n5. Exit\n')
+if Userchoice == '1':
+    print(sentence.upper())
+elif Userchoice == '2':
+    print(sentence.lower())
+elif Userchoice == '3':
+    print(sentence.title())
+elif Userchoice == '4':
+    print(sentence.strip())
+elif Userchoice == '5':
+    exit()
+sentence = input('string:')
+Userchoice = input('1. Uppercase\n2. Lowercase\n3. Titlecase\n4. Remove front and back whitespaces\n5. Exit\n')
+if Userchoice == '1':
+    print(sentence.upper())
+elif Userchoice == '2':
+    print(sentence.lower())
+elif Userchoice == '3':
+    print(sentence.title())
+elif Userchoice == '4':
+    print(sentence.strip())
+elif Userchoice == '5':
+    exit()
